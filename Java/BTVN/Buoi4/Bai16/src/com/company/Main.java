7e50b7f65bf1ae57b0f1c5d346a3afb948315a14
legianha <doantrihung592@gmail.com>
2020-09-21 22:11:57 +0700

    add java

--- Java/BTVN/Buoi4/Bai16/src/com/company/Main.java
+++ Java/BTVN/Buoi4/Bai16/src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Stock stock = new Stock("Công ty CP Chứng khoán SSI", "SSI");
+	    stock.setPreviousClosingPrice(34.5);
+	    stock.setCurrentPrice(34.35);
+	    stock.getChangePercent();
     }
 }
--- Java/BTVN/Buoi4/Bai16/src/com/company/Stock.java
+++ Java/BTVN/Buoi4/Bai16/src/com/company/Stock.java
@@ -1,5 +1,23 @@
 package com.company;
 
 public class Stock {
-    
+    public String name, code;
+    public double previousClosingPrice, currentPrice;
+
+    Stock(String new_name, String new_code){
+        name = new_name;
+        code = new_code;
+    }
+
+    public void setPreviousClosingPrice(double price){
+        previousClosingPrice = price;
+    }
+
+    public void setCurrentPrice(double price){
+        currentPrice = price;
+    }
+
+    public void getChangePercent(){
+        System.out.println("" + (previousClosingPrice/currentPrice*100) + "%");
+    }
 }
