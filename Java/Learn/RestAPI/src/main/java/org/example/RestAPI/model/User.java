b9c6c8ba115ab06f6763bf6187ad34e3f932879f
legianha <doantrihung592@gmail.com>
2021-02-14 01:01:43 +0700

    build user class

--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/model/User.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/model/User.java
@@ -1,6 +1,18 @@
 package org.example.RestAPI.model;
 
+import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Date;
 
+@Entity
+@Table
+@Data
 public class User {
+    private long id;
+    private String user_name;
+    private String password;
+    private Date created_date;
+    private Date modified_date;
 }
