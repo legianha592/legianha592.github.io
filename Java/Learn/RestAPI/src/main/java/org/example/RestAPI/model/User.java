e2da2b145bd56fd8d2bf6234ff1b6b170f60aa37
legianha <doantrihung592@gmail.com>
2021-02-14 09:49:06 +0700

    build user

--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/model/User.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/model/User.java
@@ -1,6 +1,8 @@
 package org.example.RestAPI.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -8,6 +10,8 @@ import java.util.Date;
 @Entity
 @Table
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
