639c3740fab793e7a57441e3a81cba29e7c766e5
legianha <doantrihung592@gmail.com>
2021-02-14 09:58:58 +0700

    build wallet and record

--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/model/Record.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/model/Record.java
@@ -1,4 +1,18 @@
 package org.example.RestAPI.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Record {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
 }
--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/model/Wallet.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/model/Wallet.java
@@ -1,4 +1,21 @@
 package org.example.RestAPI.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Wallet {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String wallet_name;
+    private Date created_date;
+    private Date modified_date;
 }
