7f10e993bd81315465ee5870e1c4d56d33faf9b3
legianha <doantrihung592@gmail.com>
2021-02-16 10:10:41 +0700

    fix properties

---
+++ Java/Learn/RestAPI/target/classes/record.sql
@@ -0,0 +1,3 @@
+insert into record (id, amount, title, note, created_date, modified_date) values (1, 100000, 'Bob', 'add money', '2021-02-14', '2021-02-14');
+insert into record (id, amount, title, note, created_date, modified_date) values (2, -20000, 'Bob', 'add money', '2021-02-14', '2021-02-14');
+insert into record (id, amount, title, note, created_date, modified_date) values (3, 150000, 'Bob', 'add money', '2021-02-14', '2021-02-14');
---
+++ Java/Learn/RestAPI/target/classes/wallet.sql
@@ -0,0 +1,3 @@
+insert into wallet (id, wallet_name, created_date, modified_date) values (1, 'Wallet1', '2020-02-14', '2021-02-14');
+insert into wallet (id, wallet_name, created_date, modified_date) values (2, 'Wallet2', '2020-02-14', '2021-02-14');
+insert into wallet (id, wallet_name, created_date, modified_date) values (3, 'Wallet3', '2020-02-14', '2021-02-14');
