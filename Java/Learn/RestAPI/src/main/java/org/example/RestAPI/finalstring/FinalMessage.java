592d016802cf1bf0b3db3dbc1a9b998fa0cc7889
legianha <doantrihung592@gmail.com>
2021-02-20 23:45:59 +0700

    fix request

--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/finalstring/FinalMessage.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/finalstring/FinalMessage.java
@@ -1,10 +1,18 @@
 package org.example.RestAPI.finalstring;
 
 public class FinalMessage {
+    //Đăng kí
     public static final String SIGNUP_SUCCESS = "Đăng kí thành công";
+    public static final String INVALID_USERNAME_LENGTH = "Độ dài tên không hợp lệ";
+    public static final String INVALID_USERNAME_VALUE = "Tên chứa kí tự không hợp lệ";
+    public static final String INVALID_PASSWORD_LENGTH = "Độ dài mật khẩu không hợp lệ";
+    public static final String INVALID_PASSWORD_VALUE = "Mật khẩu chứa kí tự không hợp lệ";
+
     public static final String USERNAME_EXISTED = "Tên đăng kí đã tồn tại";
+
     public static final String LOGIN_SUCCESS = "Đăng nhập thành công";
     public static final String NO_USER = "Không tồn tại người dùng";
+
     public static final String WRONG_PASSWORD = "Sai mật khẩu";
     public static final String CREATE_WALLET_SUCCESS = "Tạo ví thành công";
 }
--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/request/user/SignupRequest.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/request/user/SignupRequest.java
@@ -1,9 +1,25 @@
 package org.example.RestAPI.request.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 public class SignupRequest {
+    private final int MAX_LENGTH = 20;
+    private final int MIN_LENGTH = 6;
     private String user_name;
     private String password;
+    private String result;
+
+    public SignupRequest(String user_name, String password) {
+        this.user_name = user_name;
+        this.password = password;
+        this.checkValidRequest();
+    }
+
+    private void checkValidRequest(){
+        if (this.user_name.length() > MAX_LENGTH || this.user_name.length() < MIN_LENGTH){
+            result = new String("")
+        }
+    }
 }
