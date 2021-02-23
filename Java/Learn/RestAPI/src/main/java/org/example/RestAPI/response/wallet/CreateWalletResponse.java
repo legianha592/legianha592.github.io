884cba5e165b560f62c04d41c51c20feac9de769
legianha <doantrihung592@gmail.com>
2021-02-23 10:20:44 +0700

    add response

--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/response/wallet/CreateWalletResponse.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/response/wallet/CreateWalletResponse.java
@@ -1,4 +1,12 @@
 package org.example.RestAPI.response.wallet;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateWalletResponse {
+    private long wallet_id;
 }
