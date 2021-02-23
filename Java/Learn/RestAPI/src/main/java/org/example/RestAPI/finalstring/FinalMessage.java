cf74f9662fe95d74bb35510029a06f082164a082
legianha <doantrihung592@gmail.com>
2021-02-23 11:12:32 +0700

    fix request

--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/finalstring/FinalMessage.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/finalstring/FinalMessage.java
@@ -10,12 +10,12 @@ public class FinalMessage {
     public static final String INVALID_NEW_PASSWORD_LENGTH = "Độ dài mật khẩu mới không hợp lệ";
     public static final String INVALID_NEW_PASSWORD_VALUE = "Mật khẩu mới chứa kí tự không hợp lệ";
     public static final String CHANGE_PASSWORD_SUCCESS = "Đổi mật khẩu thành công";
-
     public static final String USERNAME_EXISTED = "Tên đăng kí đã tồn tại";
-
+    //Đăng nhập
     public static final String LOGIN_SUCCESS = "Đăng nhập thành công";
     public static final String NO_USER = "Không tồn tại người dùng";
-
+    //Tạo ví
     public static final String WRONG_PASSWORD = "Sai mật khẩu";
     public static final String CREATE_WALLET_SUCCESS = "Tạo ví thành công";
+    public static final String INVALID_WALLET_NAME_LENGTH = "Độ dài của tên ví không hợp lệ";
 }
--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/request/wallet/CreateWalletRequest.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/request/wallet/CreateWalletRequest.java
@@ -1,9 +1,26 @@
 package org.example.RestAPI.request.wallet;
 
 import lombok.Data;
+import org.example.RestAPI.finalstring.FinalMessage;
 
 @Data
 public class CreateWalletRequest {
+    private final int MAX_LENGTH = 50;
     private long user_id;
     private String wallet_name;
+    private String result;
+
+    public CreateWalletRequest(long user_id, String wallet_name) {
+        this.user_id = user_id;
+        this.wallet_name = wallet_name;
+    }
+
+    private void checkValidRequest(){
+        if (wallet_name.length() > MAX_LENGTH){
+            this.result = FinalMessage.INVALID_WALLET_NAME_LENGTH;
+        }
+        else{
+            this.result = new String("OK");
+        }
+    }
 }
