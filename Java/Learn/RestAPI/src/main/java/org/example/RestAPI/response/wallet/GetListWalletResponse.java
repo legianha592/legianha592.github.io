fa06c86da84a49065a1a258cc0fd93d5aaa552c2
legianha <doantrihung592@gmail.com>
2021-02-25 09:58:36 +0700

    add response

--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/response/wallet/GetListWalletResponse.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/response/wallet/GetListWalletResponse.java
@@ -1,2 +1,32 @@
-package org.example.RestAPI.response.wallet;public class GetListWalletResponse {
+package org.example.RestAPI.response.wallet;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.example.RestAPI.model.User;
+import org.example.RestAPI.model.Wallet;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class GetListWalletResponse {
+    @Data
+    @AllArgsConstructor
+    class MyWallet{
+        long id;
+        String wallet_name;
+        LocalDateTime created_date;
+        LocalDateTime modified_date;
+        double total_amount;
+    }
+
+    public GetListWalletResponse(User user){
+        long user_id = user.getId();
+        List<Wallet> list;
+    }
+
+
+    private double total_amount;
+    private List<MyWallet> list_wallet = new ArrayList<>();
 }
