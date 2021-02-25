c0bf1a931b6309f4d3186b7f7d99fb1ea7ea21c1
legianha <doantrihung592@gmail.com>
2021-02-25 10:01:49 +0700

    add service

--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/service/IWalletService.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/service/IWalletService.java
@@ -2,10 +2,13 @@ package org.example.RestAPI.service;
 
 import org.example.RestAPI.model.Wallet;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IWalletService {
     void addWallet(Wallet wallet);
 
     Optional<Wallet> findById(long id);
+
+    List<Wallet> findByUser_id(long id);
 }
