c2bed4975db1ddc84e65954379eacd45de745366
legianha <doantrihung592@gmail.com>
2021-02-25 10:14:23 +0700

    add response

--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/response/wallet/GetListWalletResponse.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/response/wallet/GetListWalletResponse.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.example.RestAPI.model.User;
 import org.example.RestAPI.model.Wallet;
+import org.example.RestAPI.response.user.LoginResponse;
 import org.example.RestAPI.service.IWalletService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,9 +14,6 @@ import java.util.List;
 
 @Data
 public class GetListWalletResponse {
-    @Autowired
-    IWalletService walletService;
-
     @Data
     @AllArgsConstructor
     class MyWallet{
@@ -27,11 +25,20 @@ public class GetListWalletResponse {
     }
 
     public GetListWalletResponse(User user){
-        long user_id = user.getId();
-        List<Wallet> list = walletService.findByUser_id(user_id);
+        this.addList_wallet(user);
     }
-
 
     private double total_amount;
     private List<MyWallet> list_wallet = new ArrayList<>();
+
+    private void addList_wallet(User user){
+        List<Wallet> list = user.getListWallet();
+        for (int i = 0; i < list.size(); i++){
+            Wallet wallet = list.get(i);
+            MyWallet myWallet = new MyWallet(wallet.getId(), wallet.getWallet_name(),
+                    wallet.getCreated_date(), wallet.getModified_date(), wallet.getTotal_amount());
+            list_wallet.add(myWallet);
+            this.total_amount += myWallet.total_amount;
+        }
+    }
 }
