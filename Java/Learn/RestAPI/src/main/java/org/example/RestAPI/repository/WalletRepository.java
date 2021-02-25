203ff4068ed89d0f0d99ac8eb25a8b081a9f7860
legianha <doantrihung592@gmail.com>
2021-02-25 10:06:27 +0700

    add repo

--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/repository/WalletRepository.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/repository/WalletRepository.java
@@ -2,9 +2,15 @@ package org.example.RestAPI.repository;
 
 import org.example.RestAPI.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
 
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
-
+    @Query(value = "SELECT * FROM WALLET WHERE user_id = :user_id", nativeQuery = true)
+    List<Wallet> findByUser_id(@Param("user_id") long user_id);
 }
