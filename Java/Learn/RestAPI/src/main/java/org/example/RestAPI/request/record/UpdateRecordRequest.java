5408e4c2d0bcc2ee5a39e9f118812689fdabfadf
legianha <doantrihung592@gmail.com>
2021-02-28 22:05:36 +0700

    add request

--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/request/record/UpdateRecordRequest.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/request/record/UpdateRecordRequest.java
@@ -1,8 +1,34 @@
 package org.example.RestAPI.request.record;
 
+import lombok.Data;
+import org.example.RestAPI.finalstring.FinalMessage;
+
+@Data
 public class UpdateRecordRequest {
+    private final int MAX_LENGTH = 50;
     private long record_id;
     private String title;
     private String note;
     private double amount;
+    private String result;
+
+    public UpdateRecordRequest(long record_id, String title, String note, double amount) {
+        this.record_id = record_id;
+        this.title = title;
+        this.note = note;
+        this.amount = amount;
+        this.checkValidRequest();
+    }
+
+    private void checkValidRequest(){
+        if (title.length() > MAX_LENGTH){
+            result = FinalMessage.INVALID_TITLE_RECORD_LENGTH;
+            return;
+        }
+        if (note.length() > MAX_LENGTH){
+            result = FinalMessage.INVALID_NOTE_RECORD_LENGTH;
+            return;
+        }
+        result = new String("OK");
+    }
 }
