b30945bfc88a4e9ee1104f4b19c148fff452abf3
legianha <doantrihung592@gmail.com>
2021-02-28 21:44:49 +0700

    add request

--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/request/record/UpdateRecordRequest.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/request/record/UpdateRecordRequest.java
@@ -1,4 +1,8 @@
 package org.example.RestAPI.request.record;
 
 public class UpdateRecordRequest {
+    private long record_id;
+    private String title;
+    private String note;
+    private double amount;
 }
