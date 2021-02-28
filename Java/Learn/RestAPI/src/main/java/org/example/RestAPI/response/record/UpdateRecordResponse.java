028f78b47fb677fa8a3a4fdab69443893c3ff7a2
legianha <doantrihung592@gmail.com>
2021-02-28 21:47:46 +0700

    add response

--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/response/record/UpdateRecordResponse.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/response/record/UpdateRecordResponse.java
@@ -1,4 +1,9 @@
 package org.example.RestAPI.response.record;
 
 public class UpdateRecordResponse {
+    private long record_id;
+    private String title;
+    private String note;
+    private double amount;
+    private long wallet_id;
 }
