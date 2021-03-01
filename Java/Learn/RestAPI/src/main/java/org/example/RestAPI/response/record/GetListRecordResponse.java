3d8a84d2f89e228f60fadad25f8c738d94817a6c
legianha <doantrihung592@gmail.com>
2021-03-01 09:41:25 +0700

    add response

--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/response/record/GetListRecordResponse.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/response/record/GetListRecordResponse.java
@@ -2,8 +2,11 @@ package org.example.RestAPI.response.record;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.example.RestAPI.model.Wallet;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class GetListRecordResponse {
@@ -16,5 +19,16 @@ public class GetListRecordResponse {
         LocalDateTime created_date;
         LocalDateTime modified_date;
         double amount;
+    }
+
+    public GetListRecordResponse(Wallet wallet){
+        this.addList_record(wallet);
+        this.total_amount = wallet.getTotal_amount();
+    }
+    private double total_amount;
+    private List<MyRecord> list_record = new ArrayList<>();
+
+    private void addList_record(Wallet wallet){
+
     }
 }
