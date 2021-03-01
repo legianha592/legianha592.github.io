63a376e368dbfe0693675dc3fbe17e2c3ff5d09a
legianha <doantrihung592@gmail.com>
2021-03-01 09:36:47 +0700

    add response

--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/response/record/GetListRecordResponse.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/response/record/GetListRecordResponse.java
@@ -1,4 +1,20 @@
 package org.example.RestAPI.response.record;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class GetListRecordResponse {
+    @Data
+    @AllArgsConstructor
+    class MyRecord{
+        long id;
+        String title;
+        String note;
+        LocalDateTime created_date;
+        LocalDateTime modified_date;
+        double amount;
+    }
 }
