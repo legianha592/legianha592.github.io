5429ef4ac88879f41ae2ed6ea877ba9aa2d63096
legianha <doantrihung592@gmail.com>
2021-03-09 09:58:35 +0700

    add request

--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/request/wallet/DeleteWalletRequest.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/request/wallet/DeleteWalletRequest.java
@@ -1,8 +1,10 @@
 package org.example.RestAPI.request.wallet;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class DeleteWalletRequest {
     private long wallet_id;
 }
