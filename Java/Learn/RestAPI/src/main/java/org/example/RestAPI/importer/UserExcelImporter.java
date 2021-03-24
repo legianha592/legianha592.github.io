7db07695f0b0fd24f011639fad7b90f9b02a1c4e
legianha592 <doantrihung592@gmail.com>
2021-03-24 14:55:00 +0700

    add excel importer

---
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/importer/UserExcelImporter.java
@@ -0,0 +1,102 @@
+package org.example.RestAPI.importer;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.example.RestAPI.model.User;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+public class UserExcelImporter {
+    public static String TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    static String[] HEADERs = { "Id", "User name", "Password", "Created date", "Modified date"};
+    static String SHEET = "Tutorials";
+
+    public static boolean hasExcelFormat(MultipartFile file) {
+        if (!TYPE.equals(file.getContentType())) {
+            return false;
+        }
+        return true;
+    }
+
+    public static List<User> ExcelToUserEntity(InputStream is) {
+        try {
+            Workbook workbook = new XSSFWorkbook(is);
+
+            Sheet sheet = workbook.getSheet(SHEET);
+            Iterator<Row> rows = sheet.iterator();
+
+            List<User> listUser = new ArrayList<User>();
+
+            int rowNumber = 0;
+            while (rows.hasNext()) {
+                Row currentRow = rows.next();
+
+                // skip header
+                if (rowNumber == 0) {
+                    rowNumber++;
+                    continue;
+                }
+
+                Iterator<Cell> cellsInRow = currentRow.iterator();
+
+                User user = new User();
+
+                int cellIdx = 0;
+                while (cellsInRow.hasNext()) {
+                    Cell currentCell = cellsInRow.next();
+
+                    switch (cellIdx) {
+                        case 0:
+                            user.setId((long) currentCell.getNumericCellValue());
+                            break;
+
+                        case 1:
+                            user.setUser_name(currentCell.getStringCellValue());
+                            break;
+
+                        case 2:
+                            user.setPassword(currentCell.getStringCellValue());
+                            break;
+
+                        case 3:
+                            user.setCreated_date(convertToLocalDateTime(currentCell.getDateCellValue()));
+                            break;
+
+                        case 4:
+                            user.setModified_date(convertToLocalDateTime(currentCell.getDateCellValue()));
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                    cellIdx++;
+                }
+
+                listUser.add(user);
+            }
+
+            workbook.close();
+
+            return listUser;
+        } catch (IOException e) {
+            throw new RuntimeException("fail to parse Excel file: " + e.getMessage());
+        }
+    }
+
+    private static LocalDateTime convertToLocalDateTime(Date dateToConvert) {
+        return new java.sql.Timestamp(
+                dateToConvert.getTime()).toLocalDateTime();
+    }
+}
