5645504726deb3ce459f01547f0bd54beb71ecf6
legianha592 <doantrihung592@gmail.com>
2021-03-25 09:21:57 +0700

    add wallet excel response

---
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/response/importer/WalletExcelImporterResponse.java
@@ -0,0 +1,10 @@
+package org.example.RestAPI.response.importer;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class WalletExcelImporterResponse {
+    private String message;
+}
