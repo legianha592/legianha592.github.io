e04d74f3d32ab5c60c7b3b51c9f69a4139cee327
legianha592 <doantrihung592@gmail.com>
2021-03-25 08:57:11 +0700

    add wallet exporter

---
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/exporter/WalletExcelExporter.java
@@ -0,0 +1,52 @@
+package org.example.RestAPI.exporter;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.example.RestAPI.model.User;
+import org.example.RestAPI.model.Wallet;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+
+public class WalletExcelExporter {
+    public static String TYPE = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+    static String[] HEADERs = { "Id", "Wallet name", "Created date", "Modified date", "Total amount", "User Id"};
+    static String SHEET = "Wallet";
+
+    public static ByteArrayInputStream UserEntityToExcel(List<Wallet> listWallet){
+        try (Workbook workbook = new XSSFWorkbook(); ByteArrayOutputStream out = new ByteArrayOutputStream();) {
+            Sheet sheet = workbook.createSheet(SHEET);
+
+            // Header
+            Row headerRow = sheet.createRow(0);
+
+            for (int col = 0; col < HEADERs.length; col++) {
+                Cell cell = headerRow.createCell(col);
+                cell.setCellValue(HEADERs[col]);
+            }
+
+            int rowIdx = 1;
+            for (Wallet wallet : listWallet) {
+                Row row = sheet.createRow(rowIdx++);
+
+                row.createCell(0).setCellValue(wallet.getId());
+                row.createCell(1).setCellValue(wallet.getWallet_name());
+                row.createCell(2).setCellValue(wallet.getCreated_date().toString());
+                row.createCell(3).setCellValue(wallet.getModified_date().toString());
+                row.createCell(4).setCellValue(wallet.getTotal_amount());
+                row.createCell(5).setCellValue(wallet.getUser().getId());
+                
+            }
+
+            workbook.write(out);
+            return new ByteArrayInputStream(out.toByteArray());
+        } catch (IOException e) {
+            throw new RuntimeException("fail to import data to Excel file: " + e.getMessage());
+        }
+    }
+}
