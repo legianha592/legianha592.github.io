cc50a5b7dcc2ebbfa5e6a51593e1b162ba1fd41a
legianha592 <doantrihung592@gmail.com>
2021-03-30 09:30:16 +0700

    add update type record req - res

--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/request/typerecord/UpdateTypeRecordRequest.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/request/typerecord/UpdateTypeRecordRequest.java
@@ -1,4 +1,28 @@
 package org.example.RestAPI.request.typerecord;
 
+import lombok.Data;
+import org.example.RestAPI.finalstring.FinalMessage;
+
+@Data
 public class UpdateTypeRecordRequest {
+    private final int MAX_LENGTH = 50;
+    private long typeRecord_id;
+    private String typeRecord_name;
+    private String image_url;
+    private String result;
+
+    public UpdateTypeRecordRequest(long typeRecord_id, String typeRecord_name, String image_url){
+        this.typeRecord_id = typeRecord_id;
+        this.typeRecord_name = typeRecord_name;
+        this.image_url = image_url;
+        this.checkValidRequest();
+    }
+
+    private void checkValidRequest(){
+        if (this.typeRecord_name.length() > MAX_LENGTH){
+            result = FinalMessage.INVALID_TYPERECORD_NAME_LENGTH;
+            return;
+        }
+        result = new String("OK");
+    }
 }
--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/response/typerecord/UpdateTypeRecordResponse.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/response/typerecord/UpdateTypeRecordResponse.java
@@ -1,4 +1,12 @@
 package org.example.RestAPI.response.typerecord;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class UpdateTypeRecordResponse {
+    private long typeRecord_id;
+    private String typeRecord_name;
+    private String image_url;
 }
