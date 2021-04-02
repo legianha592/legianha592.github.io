7f90da340a10c96840201daabe62ce8edaf62cfe
legianha592 <doantrihung592@gmail.com>
2021-04-02 15:03:51 +0700

    fix delete type record res

--- Java/Learn/RestAPI/src/main/java/org/example/RestAPI/response/typerecord/DeleteTypeRecordResponse.java
+++ Java/Learn/RestAPI/src/main/java/org/example/RestAPI/response/typerecord/DeleteTypeRecordResponse.java
@@ -1,7 +1,9 @@
 package org.example.RestAPI.response.typerecord;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
 @AllArgsConstructor
 public class DeleteTypeRecordResponse {
     private long typeRecord_id;
